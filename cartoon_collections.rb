95606a28e13c7ca70a798a990f18e3f1835c2389
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-29 22:19:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item,index|
+    puts "#{index +1}:#{item}" 
+  end
+  array 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |array|
+    puts "#{array.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
